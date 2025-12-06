5bdcc02c60e902a57b35aca3d05d6861cb84e237
Mohigul <mohigul.axv@gmail.com>
2025-12-06 17:41:24 +0500

    Add files via upload

---
+++ funksiyalar bo'yicha masalalar.py
@@ -0,0 +1,27 @@
+# a = "1-masala: Sonning kvadratini hisoblash"
+# print(a)
+# def kvadrat(son):
+#     natija = son * raqam
+#     return f"Kvadrat: {natija}"
+# raqam = int(input("Sonni kiriting: "))
+# print(kvadrat(raqam))
+
+a = "2-masala: Matinni takrorlash"
+print(a)
+def takrorla(matn, son):
+    natija = matn * son
+    return f"mana: {natija}"
+matn = int(input("matn kiriting:"))
+son = int(input("son kiriting:"))
+print(takrorla(matn, son))
+
+# a = "4-masala: ikki sonning yig'indisi"
+# print(a)
+# def yigindi(son1, son2):
+#     natija = son2 + son1
+#     return f"Yig'indi: {natija}"
+#
+# son1 = int(input("1-sonni kiriting: "))
+# son2 = int(input("2-sonni kiriting: "))
+#
+# print(yigindi(son1, son2))
